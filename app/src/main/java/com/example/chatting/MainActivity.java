4de0f371001080a21b5b925c57acce6f0e7944cd
Lusiana Diyan Ningrum <lusianadiyan25@gmail.com>
2020-06-16 20:04:03 +0700

    first commit

---
+++ app/src/main/java/com/example/chatting/MainActivity.java
@@ -0,0 +1,151 @@
+package com.example.chatting;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+public class MainActivity extends AppCompatActivity {
+    int SIGN_IN_REQUEST_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .build(),
+                    SIGN_IN_REQUEST_CODE
+            );
+        } else {
+            Toast.makeText(this,
+                    "Hello " + FirebaseAuth.getInstance()
+                            .getCurrentUser()
+                            .getDisplayName(),
+                    Toast.LENGTH_LONG)
+                    .show();
+            displayMessages();
+        }
+
+        FloatingActionButton fab =
+                (FloatingActionButton)findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = (EditText)findViewById(R.id.input);
+                FirebaseDatabase.getInstance()
+                        .getReference()
+                        .push()
+                        .setValue(new MessageModel(input.getText().toString(),
+                                FirebaseAuth.getInstance()
+                                        .getCurrentUser()
+                                        .getDisplayName())
+                        );
+                input.setText("");
+                Log.d(input.getText());
+            }
+        });
+
+    }
+
+    public void displayMessages(){
+        ListView listOfMessages = (ListView)findViewById(R.id.list_of_messages);
+
+        Query query = FirebaseDatabase.getInstance().getReference().child("chats");
+
+        FirebaseListOptions<MessageModel> options =
+                new FirebaseListOptions.Builder<MessageModel>()
+                        .setQuery(query, MessageModel.class)
+                        .setLayout(android.R.layout.simple_list_item_1)
+                        .build();
+
+        /*FirebaseListAdapter<MessageModel> adapter = new FirebaseListAdapter<MessageModel>(this, MessageModel.class,
+                R.layout.activity_message, FirebaseDatabase.getInstance().getReference()) {*/
+
+        FirebaseListAdapter<MessageModel> adapter = new FirebaseListAdapter<MessageModel>(options) {
+            @Override
+            protected void populateView(View v, MessageModel model, int position) {
+                TextView messageText = (TextView) v.findViewById(R.id.message_text);
+                TextView messageUser = (TextView) v.findViewById(R.id.message_user);
+                TextView messageTime = (TextView) v.findViewById(R.id.message_time);
+                messageText.setText(model.getMessageText());
+                messageUser.setText(model.getMessageUser());
+                messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)",
+                        model.getMessageTime()));
+            }
+        };
+
+        listOfMessages.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SIGN_IN_REQUEST_CODE) {
+            if(resultCode == RESULT_OK) {
+                Toast.makeText(this,
+                        "Success!",
+                        Toast.LENGTH_LONG)
+                        .show();
+                displayMessages();
+            } else {
+                Toast.makeText(this,
+                        "Please try again later.",
+                        Toast.LENGTH_LONG)
+                        .show();
+                finish();
+            }
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.menu_sign_out) {
+            AuthUI.getInstance().signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Toast.makeText(MainActivity.this,
+                                    "signed out.",
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                            finish();
+                        }
+                    });
+        }
+        return true;
+    }
+}
